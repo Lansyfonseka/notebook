916135ba5bb412dac35bf50325c03f1aef7184a9
lansyfonseka <twinkstriker@gmail.com>
2022-11-13 21:24:59 +0300

    add template left side menu

--- src/components/leftSideMenu/leftSideMenu.ts
+++ src/components/leftSideMenu/leftSideMenu.ts
@@ -13,16 +13,49 @@ class LeftSideMenu {
     const menu = document.createElement('div');
     menu.classList.add('menu');
 
+    menu.innerHTML = `
+    <div class="menu--header">
+            <div class="menu--header-title">Группы контактов</div>
+            <button class="menu--header-close"></button>
+          </div>
+          <div class="menu--body">
+            <div class="contact_group_line">
+              <input type="text" name="group_name" class="contact_group_line--input">
+              <button class="contact_group_line--delete"></button>
+            </div>
+            <div class="contact_group_line">
+              <input type="text" name="group_name" class="contact_group_line--input">
+              <button class="contact_group_line--delete"></button>
+            </div>
+            <div class="contact_group_line">
+              <input type="text" name="group_name" class="contact_group_line--input">
+              <button class="contact_group_line--delete"></button>
+            </div>
+          </div>
+          <div class="menu--footer">
+            <button class="menu--footer-add button hidden">Добавить</button>
+            <button class="menu--footer-save button blue">Сохранить</button>
+          </div>
+    `
+
+    const disableArae = document.createElement('div');
+    disableArae.classList.add('disable_area');
+
+    disableArae.addEventListener('click', () => {
+      this.hide();
+    })
+
     this.menuFrame = menuFrame;
     this.menu = menu;
 
     menuFrame.appendChild(menu);
+    menuFrame.appendChild(disableArae);
 
     return menuFrame;
   }
   show() {
     console.log('block')
-    this.menuFrame.style.display = 'block';
+    this.menuFrame.style.display = 'grid';
   }
   hide() {
     this.menuFrame.style.display = 'none';
